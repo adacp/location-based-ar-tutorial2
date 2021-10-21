b7c567bf4586676f881086fa3d4d34c7ba64f534
adacp <76569151+adacp@users.noreply.github.com>
2021-10-21 19:01:23 +0200

    Update script.js

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ function renderPlaces(places) {
 
        let model = document.createElement('a-entity');
        model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
-       model.setAttribute('image', './assets/magnemite/1975barriodelpilar.jpg');
+       model.setAttribute('plane', './assets/magnemite/1975barriodelpilar.jpg');
        model.setAttribute('rotation', '0 180 0');
        model.setAttribute('animation-mixer', '');
        model.setAttribute('scale', '0.5 0.5 0.5');
